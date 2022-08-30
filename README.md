b2ca4402836be0ed25138149c93f108fadbaaf52
José Antunes-Neto <joseparreiras@gmail.com>
2022-08-30 17:36:53 -0500

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,49 @@
+<p align="center">
+  <a href="" rel="noopener">
+ <img height=200px src="https://seeklogo.com/images/E/eleicoes-brasil-2022-logo-48D65377A2-seeklogo.com.png" alt="eleicoes logo"></a>
+</p>
+
+<h3 align="center">Análise de Planos de Governo - Eleicões Brasil 2022</h3>
+
+<div align="center">
+
+[![Status](https://img.shields.io/badge/status-active-success.svg)]()
+[![Platform](https://img.shields.io/twitter/follow/joseparreiras?style=social)]()
+
+<!-- [![Platform](https://img.shields.io/github/pipenv/locked/python-version/:user/:repo)](MY REPO) -->
+<!-- [![GitHub Issues](https://img.shields.io/github/issues/kylelobo/The-Documentation-Compendium.svg)](https://github.com/kylelobo/The-Documentation-Compendium/issues)
+[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/kylelobo/The-Documentation-Compendium.svg)](https://github.com/kylelobo/The-Documentation-Compendium/pulls) -->
+
+</div>
+
+---
+
+
+## 📝 Table of Contents
+
+- [Sobre](#about)
+- [Como Funciona?](#working)
+- [Ferramentas](#built_using)
+- [Autor](#authors)
+- [Fontes](#sources)
+
+## 🧐 Sobre <a name = "about"></a>
+
+Análise dos planos de governo dos candidatos a presidência nas eleicões brasileiras de 2022
+
+## 💭 Como Funciona? <a name = "working"></a>
+
+Através da ferramenta *tesseract* de optical character recognition (OCR), analiso os planos de governo de cada candidato. A partir disso, construo as imagens retratando as palavras mais comuns em cada arquivo. 
+
+## ⛏️ Ferramentas <a name = "built_using"></a>
+
+- [Tesseract](https://github.com/tesseract-ocr/tesseract): Ferramenta de OCR do Google
+- [WordCloud](https://github.com/amueller/word_cloud)
+
+## ✍️ Autor <a name = "authors"></a>
+
+- [@joseparreiras](https://github.com/joseparreiras) - Idea & Initial work
+
+## 💻 Fontes <a name = "sources"></a>
+
+- [Planos de Governo](https://www.tecmundo.com.br/mercado/245676-veja-plano-governo-completo-candidatos-presidencia-brasil.htm)
---
+++ planos_de_governo.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud
+import string
+from nltk.corpus import stopwords
+
+
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    import matplotlib.colors as colors
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
+
+
+def pdf_kwd(file):
+    from textract import process
+    from nltk.tokenize import word_tokenize
+
+    text = process(file, method='tesseract', encoding='utf_8', language='por')
+    text = text.decode('utf-8').replace('\\n', ' ').replace('\n', ' ')
+
+    token = word_tokenize(str(text))
+    token = [word.lower() for word in map(lambda x: x.lower(), token)]
+
+    keywords = [word for word in map(lambda x: x.lower(), token)]
+    keywords = ' '.join(keywords)
+    return keywords
+
+
+# List of Candidates to analyse
+candidates = ['soraya', 'simone', 'lula', 'bolsonaro', 'felipe davila', 'ciro']
+
+# Read files
+plan = {}
+
+for x in candidates:
+    file = 'planos/' + x + '.pdf'
+    plan.update({x: pdf_kwd(file)})
+
+# Words to drop
+stop_words = [
+    'brasil', 'governo', 'presidente', 'nacional', 'politica',
+    'pais', 'lei', 'leis', 'metas', 'todo', 'publica', 'publico',
+    'maior', 'menor', 'mais', 'menos', 'brasileiro',
+    'brasileira', 'varios', 'nacionais', 'sera', 'estado', 'ao',
+    'deve', 'outra', 'outro', 'sistema', 'programa', 'foco', 'todos',
+    'nao', 'sim', 'novo', 'proposta', 'federal', 'plano', 'se',
+    'bolsenaro', 'importante', 'raya', 'thronicke', 'tebet', 'mara',
+    'gabrilli', 'geraldo', 'alckmin', 'tiago', 'mitraud', 'vice',
+    'braga', 'netto', 'ana', 'paula', 'mattos', 'ndo', 'gestdo', 'tanto', 'ao', 'alem', 'além', 'já', 'ja', 'sao', 'são', 'sdo', 'cada', 'país', 'deverá', 'política', 'forma', 'bem', 'mal', 'vamos', 'pessoa', 'deverão', 'projeto', 'público', 'políticas', 'pública', 'públicas', 'públicos', 'implantar', 'meta', 'promover', 'sobretudo', 'toda', 'ss', 'meio'
+] + candidates + list(string.ascii_lowercase) + stopwords.words('portuguese') + ['(', ')', ';', ':', '[', ']', ',']
+
+
+# Mask for WordCloud
+mask = np.array(Image.open('brazil.png'))
+
+for i, x in enumerate(candidates):
+    text = plan[x]
+    cloud = WordCloud(background_color='white', stopwords=stop_words,
+                      max_words=2000,
+                      mask=mask, contour_width=0).generate(text)
+    plt.figure()
+    plt.imshow(cloud, interpolation='bilinear')
+    plt.title(x.capitalize())
+    plt.axis('off')
+    plt.savefig('imagens/'+x+'.jpeg', dpi=1200,
+                bbox_inches='tight', transparent=False)
